9dd2443f03e826a552ec12c519dfbc3d16fa0f7c
sparrowsl <benthorpe.dev@gmail.com>
2024-08-30 06:27:41 +0000

    setup with a homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<section class="grid place-content-center min-h-[70dvh] text-center">
+	<p class="text-sm border px-3 py-2 bg-white rounded-full w-fit mx-auto">
+		vote for ideas you are interested in...
+	</p>
+
+	<h1 class="text-6xl text-accent mb-4">Idea Voting Platform</h1>
+	<p class="font-light max-w-xl text-center">
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam voluptatum
+		atque error aut fugiat dolore corporis a ipsa, amet hic.
+	</p>
+
+	<div class="max-auto">
+		<a href="/ideas" class="mt-3 btn-accent text-white w-fit btn-sm btn">
+			view ideas
+		</a>
+	</div>
+</section>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+
+	theme: {
+		extend: {},
+	},
+
+	daisyui: {
+		themes: ["light"],
+	},
+
+	plugins: [
+		require("@tailwindcss/typography"),
+		// require("@tailwindcss/forms"),
+		daisyui,
+	],
+};
